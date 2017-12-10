06ebec2e7727b49361f9c2c7e55ba91de27daf70
SanjanaVK <saka2821@colorado.edu>
2017-12-10 02:53:15 -0700

    timestamp added for cache

--- proxy_server.c
+++ proxy_server.c
@@ -14,6 +14,9 @@
 #include<stddef.h>
 #include<openssl/md5.h>
 #include<sys/signal.h>
+#include<time.h>
+#include<memory.h>
+#include<dirent.h>
 
 #define MAXBUF 2048
    
@@ -23,9 +26,9 @@ perror(msg);
 exit(0);
 }
   
-char * check_ip_in_cache(char * t2)
+char * check_ip_in_cache(char * request_url)
 {
-    char * temp_t2 = t2;
+    char * temp_request_url = request_url;
     char * temp_ip = (char *) malloc(50 * sizeof(char));
     FILE *file = fopen ("ip_cache.txt", "r");
     if(file == NULL)
@@ -39,19 +42,19 @@ char * check_ip_in_cache(char * t2)
     while(fgets(line, sizeof(line), file) != NULL)
     {
     	char * token;
-    	printf("line is %s\n", line);
+    	//printf("line is %s\n", line);
     	token = strtok(line, " ");
     	if(token != NULL)
     	{
-    		printf("host name is %s\n", token);
-    		if(strcmp(token, t2) == 0)
+    		//printf("host name is %s\n", token);
+    		if(strcmp(token, request_url) == 0)
     		{
     			token = strtok(NULL, " ");
     			if(token != NULL)
     			{
-    				printf("host name is %s\n", token);
+    				//printf("host name is %s\n", token);
     				strcpy(temp_ip, token);
-    				printf("Found ip address\n");
+    				printf("Found ip address in cache\n");
     				fclose(file);
     				return temp_ip;
     			}
@@ -64,9 +67,9 @@ char * check_ip_in_cache(char * t2)
     
 }
 
-int check_ip_is_forbidden(char * t2, char * temp_ip_addr)
+int check_ip_is_forbidden(char * request_url, char * temp_ip_addr)
 {
-	char * temp_t2 = t2;
+	char * temp_request_url = request_url;
     FILE *file = fopen ("forbidden_sites.txt", "r");
     if(file == NULL)
     {
@@ -79,12 +82,12 @@ int check_ip_is_forbidden(char * t2, char * temp_ip_addr)
     while(fgets(line, sizeof(line), file) != NULL)
     {
     	char * token;
-    	printf("line is %s\n", line);
+    	//printf("line is %s\n", line);
     	token = strtok(line, "\n");
     	if(token != NULL)
     	{
-    		printf("host name or ip address is %s\n", token);
-    		if(strcmp(token, t2) == 0)
+    		//printf("host name or ip address is %s\n", token);
+    		if(strcmp(token, request_url) == 0)
     		{
     			printf("%s: It is a forbidden site\n", token);
     			fclose(file);
@@ -101,7 +104,7 @@ int check_ip_is_forbidden(char * t2, char * temp_ip_addr)
     fclose(file);
     return 0; 
 }
-int write_ip_to_cache(char * t2, char * host_addr, unsigned long int length)
+int write_ip_to_cache(char * request_url, char * host_addr, unsigned long int length)
 {
 	int fd_write;
 	fd_write = open( "ip_cache.txt", O_RDWR|O_CREAT|O_APPEND, 0666);
@@ -110,7 +113,7 @@ int write_ip_to_cache(char * t2, char * host_addr, unsigned long int length)
 		perror("File:");
 		return -1;
 	}
-	write(fd_write, t2, strlen(t2));
+	write(fd_write, request_url, strlen(request_url));
 	write(fd_write, " ", 1);
 	write(fd_write, host_addr, length);
     write(fd_write, "\n", 1);  
@@ -120,6 +123,20 @@ int write_ip_to_cache(char * t2, char * host_addr, unsigned long int length)
 
 int page_cache_present(char * filename)
 {
+	char fullpath[50] ;
+	bzero(fullpath, sizeof(fullpath));
+	strcpy(fullpath,"./cache/");
+	//printf("full path is %s\n", fullpath);
+	struct stat st = {0};
+    if (stat(fullpath, &st) == -1) 
+    {
+        mkdir(fullpath, 0700);
+        printf("directory %s is created\n", fullpath);
+    }
+    strcat(fullpath, filename);
+    bzero(filename, sizeof(filename));
+    strcpy(filename, fullpath);
+    printf("filename path is %s\n", filename);
 	FILE *temp_fp = fopen(filename, "r");
     if (temp_fp == NULL)
     {
@@ -158,7 +175,7 @@ void check_md5sum(char * url, char * md5sum_temp)
         sprintf(temp, "%0X", out[n]);
         strcat(md5sum_temp, temp);
     }
-    printf("\nmd5sum: %s\n", md5sum_temp);
+    //printf("\nmd5sum: %s\n", md5sum_temp);
     return;
 }
 int main(int argc,char* argv[])
@@ -166,7 +183,10 @@ int main(int argc,char* argv[])
 	pid_t pid;
 	struct sockaddr_in addr_in,cli_addr,serv_addr;
 	struct hostent* host;
-	int sockfd,newsockfd;
+	int sockfd,connfd;
+	time_t start_time, end_time;
+	double diff_time;
+	start_time = time(0);
    
 	if(argc<2)
 	error("./proxy <port_no>");  
@@ -195,9 +215,9 @@ int main(int argc,char* argv[])
 	int clilen=sizeof(cli_addr);
 	for(; ;)
 	{
-		newsockfd=accept(sockfd,(struct sockaddr*)&cli_addr,&clilen);
+		connfd=accept(sockfd,(struct sockaddr*)&cli_addr,&clilen);
    	
-		if(newsockfd<0)
+		if(connfd<0)
 			error("Problem in accepting connection");
   
 		pid=fork();
@@ -205,46 +225,47 @@ int main(int argc,char* argv[])
 		{
 			printf("Created a new child.....\n");
 			struct sockaddr_in host_addr;
-			int flag=0,newsockfd1,n,port=0,i,sockfd1;
-			char buffer[510],t1[300],t2[300],t3[10];
+			int flag=0,connfd1,n,port=0,i,sockfd1;
+			char buffer[510],request[300],request_url[300],request_version[10];
 			char* temp=NULL;
 			bzero((char*)buffer,500);
-			recv(newsockfd,buffer,500,0);
+			recv(connfd,buffer,500,0);
+			
    
-			sscanf(buffer,"%s %s %s",t1,t2,t3);
-            printf("received is:\n");
-            printf("t1:::%s\nt2:::%s\nt3:::%s:::\n", t1, t2, t3);
+			sscanf(buffer,"%s %s %s",request,request_url,request_version);
+            //printf("received is:\n");
+            //printf("request:::%s\nrequest_url:::%s\nrequest_version:::%s:::\n", request, request_url, request_version);
 
    
-			if(((strncmp(t1,"GET",3)==0))&&((strncmp(t3,"HTTP/1.1",8)==0)||(strncmp(t3,"HTTP/1.0",8)==0))&&(strncmp(t2,"http://",7)==0))
+			if(((strncmp(request,"GET",3)==0))&&((strncmp(request_version,"HTTP/1.1",8)==0)||(strncmp(request_version,"HTTP/1.0",8)==0))&&(strncmp(request_url,"http://",7)==0))
 			{
-				strcpy(t1,t2);
+				strcpy(request,request_url);
 				char version[10];
 				bzero(version, sizeof(version));
-				memcpy(version, t3, strlen(t3));
+				memcpy(version, request_version, strlen(request_version));
 				char url[300];
 				bzero(url, sizeof(url));
-				memcpy(url, t2, strlen(t2));
+				memcpy(url, request_url, strlen(request_url));
 				printf("version is %s\n", version);
 				printf("url is %s\n", url);
 				
 				flag=0;
    
-				for(i=7;i<strlen(t2);i++)
+				for(i=7;i<strlen(request_url);i++)
 				{
-					//if(t2[i]==':' || t2[i]=='?')
-					if(t2[i]==':')
+					//if(request_url[i]==':' || request_url[i]=='?')
+					if(request_url[i]==':')
 					{
 						flag=1;
 						break;
 					}
-					else if(t2[i] == '?' || t2[i] == '/')
+					else if(request_url[i] == '?' || request_url[i] == '/')
 					{
 						break;
 					}
 				}
    
-				temp=strtok(t2,"//");
+				temp=strtok(request_url,"//");
 				//printf("flag is %d, temp is %s\n", flag, temp);
 				if(flag==0)
 				{
@@ -258,32 +279,32 @@ int main(int argc,char* argv[])
 				//	printf("flag is 1, temp is %s\n", temp);
 				}
    
-				sprintf(t2,"%s",temp);
-				printf("host = %s",t2);
+				sprintf(request_url,"%s",temp);
+				printf("host = %s",request_url);
 				
-				char * cache_ip = check_ip_in_cache(t2); //check if site in cache
+				char * cache_ip = check_ip_in_cache(request_url); //check if site in cache
 				//printf("version is %s\n", version);
    
 				if(cache_ip == NULL)
 				{
                 	printf("\n...........getting ip address and storing in cache.....\n");
-                	host=gethostbyname(t2);
+                	host=gethostbyname(request_url);
 				
                 	if(host == NULL)
 					{
-						send(newsockfd,"SERVER NOT FOUND\n",17,0);
+						send(connfd,"SERVER NOT FOUND\n",17,0);
 						goto closing;
 					}
 					cache_ip = (char *)malloc(host->h_length * sizeof(char));
-					int result = write_ip_to_cache(t2, (char *)host->h_addr, host->h_length);
+					int result = write_ip_to_cache(request_url, (char *)host->h_addr, host->h_length);
 					if(result == -1)
 					{
 						printf("Could not create cache\n");
-						//send(newsockfd,"ERROR WHILE CACHING\n",20,0);
+						//send(connfd,"ERROR WHILE CACHING\n",20,0);
 						//goto closing;
 					}
 					strncpy(cache_ip, host->h_addr, host->h_length);
-					printf("host->h_addr is %s\n, cache_ip is %s\n", host->h_addr, cache_ip);
+					//printf("host->h_addr is %s\n, cache_ip is %s\n", host->h_addr, cache_ip);
 				}
 
                 else
@@ -300,9 +321,9 @@ int main(int argc,char* argv[])
 				}
    
    
-				strcat(t1,"^]");
-				//printf("\nt1 after adding ^] is %s\n", t1);
-				temp=strtok(t1,"//");
+				strcat(request,"^]");
+				//printf("\nrequest after adding ^] is %s\n", request);
+				temp=strtok(request,"//");
 				//printf("temp token on // is %s\n", temp);
 				temp=strtok(NULL,"/");	
 				//printf("temp token on / is %s\n", temp);
@@ -320,9 +341,9 @@ int main(int argc,char* argv[])
 				bcopy((char*)cache_ip,(char*)&host_addr.sin_addr.s_addr,strlen(cache_ip));
 				char temp_ip_addr[100];
 				strcpy(temp_ip_addr, inet_ntoa(host_addr.sin_addr));
-				if(check_ip_is_forbidden(t2, temp_ip_addr) == -1) //checking if site is forbidden
+				if(check_ip_is_forbidden(request_url, temp_ip_addr) == -1) //checking if site is forbidden
 			    {
-			    	send(newsockfd,"ERROR 403 FORBIDDEN\n",20,0);
+			    	send(connfd,"ERROR 403 FORBIDDEN\n",20,0);
 					goto closing;
 			    }
 				//bcopy((char*)host->h_addr,(char*)&host_addr.sin_addr.s_addr,host->h_length);
@@ -330,22 +351,22 @@ int main(int argc,char* argv[])
    
    
 				sockfd1=socket(AF_INET,SOCK_STREAM,IPPROTO_TCP);
-				newsockfd1=connect(sockfd1,(struct sockaddr*)&host_addr,sizeof(struct sockaddr));
-				sprintf(buffer,"\nConnected to %s  IP - %s\n",t2,inet_ntoa(host_addr.sin_addr));
-				if(newsockfd1<0)
+				connfd1=connect(sockfd1,(struct sockaddr*)&host_addr,sizeof(struct sockaddr));
+				sprintf(buffer,"\nConnected to %s  IP - %s\n",request_url,inet_ntoa(host_addr.sin_addr));
+				if(connfd1<0)
 					error("Error in connecting to remote server");
    
 				printf("\n%s\n",buffer);
-				//send(newsockfd,buffer,strlen(buffer),0);
+				//send(connfd,buffer,strlen(buffer),0);
 				bzero((char*)buffer,sizeof(buffer));
 				if(temp!=NULL)
-					sprintf(buffer,"GET /%s %s\r\nHost: %s\r\nConnection: close\r\n\r\n",temp,t3,t2);
-					//sprintf(buffer,"GET http://%s/%s %s\r\nHost: %s\r\nConnection: close\r\n\r\n",t2,temp,t3,t2);
+					sprintf(buffer,"GET /%s %s\r\nHost: %s\r\nConnection: close\r\n\r\n",temp,request_version,request_url);
+					//sprintf(buffer,"GET http://%s/%s %s\r\nHost: %s\r\nConnection: close\r\n\r\n",request_url,temp,request_version,request_url);
 				else
 				{
 					//printf("version is %s\n", version);
-					sprintf(buffer,"GET / %s\r\nHost: %s\r\nConnection: close\r\n\r\n",version,t2);
-					//sprintf(buffer,"GET http://%s %s\r\nHost: %s\r\nConnection: close\r\n\r\n",t2,t3,t2);
+					sprintf(buffer,"GET / %s\r\nHost: %s\r\nConnection: close\r\n\r\n",version,request_url);
+					//sprintf(buffer,"GET http://%s %s\r\nHost: %s\r\nConnection: close\r\n\r\n",request_url,request_version,request_url);
 				}
 					
  
@@ -359,17 +380,21 @@ int main(int argc,char* argv[])
 					char md5sum_temp[32];
 					bzero(md5sum_temp, sizeof(md5sum_temp));
 					check_md5sum(url, md5sum_temp);
-                	printf("Completed md5sum\n");
-                	printf("md5sum is %s\n", md5sum_temp);
-                	printf("size of md5sum is %d\n",strlen(md5sum_temp));
-					if(page_cache_present(md5sum_temp) == 0)
+                	//printf("Completed md5sum\n");
+                	//printf("md5sum is %s\n", md5sum_temp);
+                	//printf("size of md5sum is %d\n",strlen(md5sum_temp));
+                	char cache_filename[200];
+                	bzero(cache_filename, sizeof(cache_filename));
+                	strcpy(cache_filename, md5sum_temp);
+					if(page_cache_present(cache_filename) == 0)
 					{
 						int fd_write;
 						printf("md5sum is %s\n", md5sum_temp);
-                		printf("size of md5sum is %d\n",strlen(md5sum_temp));
-						fd_write = open( md5sum_temp, O_RDWR|O_CREAT|O_APPEND, 0666);
-						printf("md5sum is %s\n", md5sum_temp);
-                		printf("size of md5sum is %d\n",strlen(md5sum_temp));
+                	//	printf("size of md5sum is %d\n",strlen(md5sum_temp));
+                	//	printf("directory is %s\n", cache_filename);
+						fd_write = open( cache_filename, O_RDWR|O_CREAT|O_APPEND, 0666);
+					//	printf("md5sum is %s\n", md5sum_temp);
+                	//	printf("size of md5sum is %d\n",strlen(md5sum_temp));
 						if(fd_write == -1)
 						{
 							perror("File:");
@@ -383,14 +408,40 @@ int main(int argc,char* argv[])
 							if(!(n<=0))
 							{
 								write(fd_write, buffer, n);
-								send(newsockfd,buffer,n,0);
+								send(connfd,buffer,n,0);
 							}
 						}while(n>0);
+						end_time = time(0);
+						diff_time = difftime(end_time, start_time);
+						printf("diff in time is %f\n", diff_time);
+						char diff_time_char[50];
+						bzero(diff_time_char, sizeof(diff_time_char));
+						sprintf(diff_time_char, "%f", diff_time);
+						int fd2_write;
+						fd2_write = open("database.txt", O_RDWR|O_CREAT|O_APPEND, 0666);
+						
+						if(fd2_write == -1)
+						{
+							perror("File:");
+							return -1;
+						}
+						write(fd2_write, md5sum_temp, strlen(md5sum_temp));
+						write(fd2_write, " ", 1);
+						write(fd2_write, diff_time_char, strlen(diff_time_char));
+						write(fd2_write, "\n", 1);
+						close(fd2_write);
+						
+						//write(fd_write, buffer, n);
+
 					}
 					else
 					{
                     	printf("Page is present in the cache\n");
-                    	FILE *file = fopen (md5sum_temp, "r");
+                    	end_time = time(0);
+						diff_time = difftime(end_time, start_time);
+						printf("diff in time is %f\n", diff_time);
+						//check_timestamp
+                    	FILE *file = fopen (cache_filename, "r");
     					if(file == NULL)
     					{
         					perror("File not opened :");
@@ -404,28 +455,29 @@ int main(int argc,char* argv[])
     					{
     						bzero(cache_buf, sizeof(cache_buf));
     						int result = fread(cache_buf, 1, 500, file);
-    						send(newsockfd,cache_buf,result,0);
+    						send(connfd,cache_buf,result,0);
 
     					}
     					fclose(file);
                     	
 					}
 				}
-				//close(newsockfd);
+				//close(connfd);
 			}
 			else
 			{
-				send(newsockfd,"400 : BAD REQUEST\nONLY HTTP REQUESTS ALLOWED",18,0);
+				send(connfd,"400 : BAD REQUEST\nONLY HTTP REQUESTS ALLOWED",18,0);
 			}
+		    //}
 			closing:
 			close(sockfd1);
-			close(newsockfd);
+			close(connfd);
 			close(sockfd);
             _exit(0);
 		}
 		else
 		{
-			close(newsockfd);
+			close(connfd);
 		}
 	}
 	return 0;
